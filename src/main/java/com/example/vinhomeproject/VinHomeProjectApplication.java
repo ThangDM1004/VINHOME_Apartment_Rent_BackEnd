a2036795b0e6f22b9f56b6ce2ba31e17399595d7
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-01-14 11:00:56 +0700

    create project

---
+++ src/main/java/com/example/vinhomeproject/VinHomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinhomeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinHomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinHomeProjectApplication.class, args);
+    }
+    /ff
+}
